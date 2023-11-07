77e03110be6319cfb508665552ea398e22096170
Jesus Emmanuel Hernandez Garcia <hegj18.04@gmail.com>
2023-11-07 00:16:22 -0600

    Tarea de ejercicio de ecmascript lista

--- javascript/4-ecmascript/ejercicio.js
+++ javascript/4-ecmascript/ejercicio.js
@@ -10,44 +10,31 @@
  */
 
 
-class ListaNumeros {
-    constructor(numeros) {
-        this._numeros = numeros
-    }
-
-    get numeros() {
-        return this._numeros
-    }
-
-    numerosMayoresAMedia() {
-        /*
-            filter. Filtrar items
-            Se utiliza para crear un nuevo array reteniendo solo los elementos que cumplan con una condición específica
-            La condición debe devolver `true` o `false`
-        */
-       
-        // Calculo de la media
-        const media = this.numeros.reduce((a, b) => a + b, 0) / this.numeros.length
-
-        // COMPLETA AQUÍ CÓDIGO CON MÉTODO .filter
-    }
-
-    ordenamientoMayorAMenor() {
-        /*
-            sort. Ordenamiento condicionado
-            Investiga más sobre el funcionamiento de los parámetros (a,b) de sort para que lograr 
-            un ordenamiento de mayor a menor
-
-            this.numeros.sort((a, b))
-        */
-
-        // COMPLETA AQUÍ CÓDIGO CON MÉTODO .sort
-
-        console.log(this.numeros)
+    class ListaNumeros {
+        constructor(numeros) {
+            this._numeros = numeros;
+        }
+    
+        get numeros() {
+            return this._numeros;
+        }
+    
+        numerosMayoresAMedia() {
+            const media = this.numeros.reduce((a, b) => a + b, 0) / this.numeros.length;
+    
+            const numerosMayores = this.numeros.filter(numero => numero > media);
+    
+            console.log(numerosMayores);
+        }
+    
+        ordenamientoMayorAMenor() {
+            this.numeros.sort((a, b) => b - a);
+    
+            console.log(this.numeros);
+        }
     }
     
-}
-
-const misNumeros = new ListaNumeros([1,5,3,4,2,6,8,10,9,7])
-misNumeros.numerosMayoresAMedia()
-misNumeros.ordenamientoMayorAMenor()
+    const misNumeros = new ListaNumeros([1, 5, 3, 4, 2, 6, 8, 10, 9, 7]);
+    misNumeros.numerosMayoresAMedia();
+    misNumeros.ordenamientoMayorAMenor();
+    
